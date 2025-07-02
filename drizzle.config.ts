d65c38a690962b67a7d0c7830a8b5f22f3d74656
Rugemana <rugemachristian2000@gmail.com>
2025-07-02 09:13:08 +0200

    ♻️ Reinstalled dependencies after cleanup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,6 @@ export default {
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
